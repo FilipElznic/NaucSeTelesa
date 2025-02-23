64968559b69dd0d01f3f025f24beca66230d891e
Filip Elznic <elznicfilip@gmail.com>
2025-02-23 20:51:56 +0100

    updatik

--- NaucSeTelesa/src/pages/pomocPage.jsx
+++ NaucSeTelesa/src/pages/pomocPage.jsx
@@ -1,12 +1,13 @@
 import Footer from "../Components/Footer";
 import Help from "../Components/Help";
 import Navbar from "../Components/Navbar";
+import TaskLayout from "../Components/TaskLayout";
 
 function PomocPage() {
   return (
     <>
       <Navbar />
-      <Help />
+      <TaskLayout />
       <Footer />
     </>
   );
--- NaucSeTelesa/src/pages/successPage.jsx
+++ NaucSeTelesa/src/pages/successPage.jsx
@@ -23,38 +23,6 @@ function SuccessPage() {
         <LeaderboardW />
       </FadeInWrapper>
 
-      <div className="bg-black min-h-screen flex flex-col items-center p-4">
-        <div className="w-full flex flex-col gap-4 p-6 bg-white/10 backdrop-blur-lg border border-white/20 rounded-3xl">
-          {/* Top Bar */}
-          <div className="w-full h-24 bg-white/10 rounded-3xl"></div>
-
-          <div className="flex flex-col md:flex-row gap-4 w-full h-[70vh]">
-            <div className="w-full h-full md:w-3/5 bg-white/10 rounded-3xl"></div>
-            <div className="w-full h-full md:w-1/5 bg-white/10 rounded-3xl"></div>
-          </div>
-
-          {/* Main Content */}
-
-          <div className="flex flex-col md:flex-row gap-4 w-full h-[70vh]">
-            <div className="w-full h-full md:w-3/5 bg-white/10 rounded-3xl"></div>
-            <div className="w-full h-full md:w-1/5 bg-white/10 rounded-3xl"></div>
-          </div>
-
-          {/* Middle Section */}
-
-          <div className="flex flex-col md:flex-row gap-4 w-full h-[70vh]">
-            <div className="w-full h-full md:w-3/5 bg-white/10 rounded-3xl"></div>
-            <div className="w-full h-full md:w-1/5 bg-white/10 rounded-3xl"></div>
-          </div>
-
-          {/* Bottom Section */}
-
-          <div className="flex flex-col md:flex-row gap-4 w-full h-[70vh]">
-            <div className="w-full h-full md:w-3/5 bg-white/10 rounded-3xl"></div>
-            <div className="w-full h-full md:w-1/5 bg-white/10 rounded-3xl"></div>
-          </div>
-        </div>
-      </div>
       <Footer />
     </>
   );
