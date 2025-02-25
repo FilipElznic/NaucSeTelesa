de932eb5b78f21a89167e309dfd767b0af55d567
Filip Elznic <elznicfilip@gmail.com>
2025-02-25 09:17:44 +0100

    Update TaskLayout.jsx

--- NaucSeTelesa/src/Components/TaskLayout.jsx
+++ NaucSeTelesa/src/Components/TaskLayout.jsx
@@ -14,17 +14,14 @@ function TaskLayout() {
             <div className="w-full min-h-[40vh] md:min-h-[70vh] lg:w-3/5 usergradient rounded-3xl flex flex-col usergradient-glow">
               <div className="w-full flex justify-between mt-4 lg:mt-16  p-5 px-5 lg:px-10">
                 <h1 className="text-3xl md:text-4xl lg:text-5xl w-full md:w-3/4 userlvl3">
-                  Určete, které těleso má jednu podstavu a vrchol.
+                  {/* name */}
                 </h1>
                 <p className="text-md md:text-lg lg:text-2xl text-zinc-300 hidden sm:block">
-                  10 xp
+                  {/* xp */}
                 </p>
               </div>
               <div className="w-full xl:w-3/4 p-3 lg:p-10 text-md sm:text-xl md:text-2xl text-gray-500 flex-grow">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-                nihil eum? Totam veritatis quibusdam illum ducimus deleniti,
-                quae magni voluptatum omnis aliquam tempore quo sed explicabo
-                eius vero consequatur modi?
+                {/* description */}
               </div>
               <div className="w-full flex justify-between text-xl lg:text-3xl p-10 mt-auto">
                 <CiCircleInfo />
@@ -33,29 +30,62 @@ function TaskLayout() {
             </div>
             <div className="w-full h-[30vh] lg:h-[70vh] lg:w-1/5  usergradient rounded-3xl usergradient-glow">
               <div className="h-full w-full flex justify-evenly flex-row lg:flex-col items-center ">
-                <div className="w-1/3 h-1/3 md:w-48 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-4">
+                <div className="w-1/3 h-1/3 md:w-40 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-1 sm:m-4">
                   <div className="h-full w-full usergradient rounded-3xl flex justify-center items-center usergradient-glow">
-                    <p className="text-xl md:text-3xl">Koule</p>
+                    <p className="text-xl md:text-3xl"> {/* answera */}</p>
                   </div>
                 </div>
-                <div className="w-1/3 h-1/3 md:w-48 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-4">
+                <div className="w-1/3 h-1/3 md:w-40 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-1 sm:m-4">
                   <div className="h-full w-full usergradient rounded-3xl flex justify-center items-center usergradient-glow ">
-                    <p className="text-xl md:text-3xl">Koule</p>
+                    <p className="text-xl md:text-3xl">{/* answerb */}</p>
                   </div>
                 </div>
-                <div className="w-1/3 h-1/3 md:w-48 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-4">
+                <div className="w-1/3 h-1/3 md:w-40 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-1 sm:m-4">
                   <div className="h-full w-full usergradient rounded-3xl flex justify-center items-center usergradient-glow ">
-                    <p className="text-xl md:text-3xl">Koule</p>
+                    <p className="text-xl md:text-3xl">{/* answerc */}</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          {/* Main Content */}
-          <div className="flex flex-col md:flex-row gap-4 w-full h-[70vh]">
-            <div className="w-full h-full md:w-3/5 usergradient rounded-3xl usergradient-glow"></div>
-            <div className="w-full h-full md:w-1/5 usergradient rounded-3xl usergradient-glow"></div>
+          <div className="flex flex-col lg:flex-row gap-4 w-full min-h[70vh]">
+            <div className="w-full min-h-[40vh] md:min-h-[70vh] lg:w-3/5 usergradient rounded-3xl flex flex-col usergradient-glow">
+              <div className="w-full flex justify-between mt-4 lg:mt-16  p-5 px-5 lg:px-10">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl w-full md:w-3/4 userlvl3">
+                  {/* name */}
+                </h1>
+                <p className="text-md md:text-lg lg:text-2xl text-zinc-300 hidden sm:block">
+                  {/* xp */}
+                </p>
+              </div>
+              <div className="w-full xl:w-3/4 p-3 lg:p-10 text-md sm:text-xl md:text-2xl text-gray-500 flex-grow">
+                {/* description */}
+              </div>
+              <div className="w-full flex justify-between text-xl lg:text-3xl p-10 mt-auto">
+                <CiCircleInfo />
+                <CiHeart />
+              </div>
+            </div>
+            <div className="w-full h-[30vh] lg:h-[70vh] lg:w-1/5  usergradient rounded-3xl usergradient-glow">
+              <div className="h-full w-full flex justify-evenly flex-row lg:flex-col items-center ">
+                <div className="w-1/3 h-1/3 md:w-40 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-1 sm:m-4">
+                  <div className="h-full w-full usergradient rounded-3xl flex justify-center items-center usergradient-glow">
+                    <p className="text-xl md:text-3xl"> {/* answera */}</p>
+                  </div>
+                </div>
+                <div className="w-1/3 h-1/3 md:w-40 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-1 sm:m-4">
+                  <div className="h-full w-full usergradient rounded-3xl flex justify-center items-center usergradient-glow ">
+                    <p className="text-xl md:text-3xl">{/* answerb */}</p>
+                  </div>
+                </div>
+                <div className="w-1/3 h-1/3 md:w-40 md:h-28  rounded-3xl items-center flex flex-col md:flex-row m-1 sm:m-4">
+                  <div className="h-full w-full usergradient rounded-3xl flex justify-center items-center usergradient-glow ">
+                    <p className="text-xl md:text-3xl">{/* answerc */}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
