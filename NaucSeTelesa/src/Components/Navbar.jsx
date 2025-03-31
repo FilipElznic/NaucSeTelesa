a8fa92635815eeac930b15ef7bb315089899adac
Filip Elznic <elznicfilip@gmail.com>
2025-03-31 12:07:36 +0200

    Update Navbar.jsx

--- NaucSeTelesa/src/Components/Navbar.jsx
+++ NaucSeTelesa/src/Components/Navbar.jsx
@@ -71,63 +71,65 @@ function Navbar() {
   }, [isDropdownOpen]);
 
   return (
-    <nav className="bg-transparent text-white px-4 py-2 flex justify-between items-center shadow-lg relative z-30">
-      {/* Left Side: Collapsible Menu */}
-      <div className="flex items-center">
-        {/* Hamburger Icon */}
-        <button
-          onClick={toggleMenu}
-          className="lg:hidden text-white focus:outline-none transition-transform transform duration-300 z-40"
-        >
-          <svg
-            className="w-7 h-7"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
-          </svg>
-        </button>
+    <div className="flex h-20 w-full ">
+      {/* Full height sidebar for mobile (left side) */}
+      <div
+        className={`fixed inset-y-0 left-0 w-64 bg-gray-900 usergradient shadow-lg transition-transform transform z-40 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } lg:hidden`}
+      >
+        <div className="flex flex-col h-full py-6">
+          <div className="px-4 mb-8">
+            <button
+              onClick={toggleMenu}
+              className="text-white focus:outline-none transition-transform transform duration-300"
+            >
+              <svg
+                className="w-7 h-7"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
 
-        <div
-          className={`fixed top-16 left-5 w-60 rounded-3xl transition-all duration-200 ease-in-out z-20 ${
-            isMenuOpen
-              ? "opacity-100 scale-100 usergradient shadow-lg pointer-events-auto"
-              : "opacity-0 scale-95 pointer-events-none"
-          } lg:static lg:block lg:w-auto lg:opacity-100 lg:scale-100 lg:usergradient-none lg:pointer-events-auto`}
-        >
-          <ul className="lg:flex lg:space-x-6 space-y-6 lg:space-y-0 p-4 rounded-2xl font-bold">
-            <li className="flex items-center ">
+          <ul className="space-y-6 px-4 flex-1 font-bold">
+            <li className="flex items-center">
               <Link
                 to="/success"
-                className="flex text-white text-2xl w-full px-4"
+                className="flex text-white text-2xl w-full px-4 items-center "
                 onClick={closeMenu}
               >
-                <FaHome className="mr-2" />
+                {/*<FaHome className="mr-2" />*/}
+                Domů
               </Link>
             </li>
 
-            <li className="">
+            <li>
               <Link
                 to="/telesa"
                 className="block w-full text-white text-xl px-4"
                 onClick={closeMenu}
               >
+                {/*<AiOutlineAppstore className="mr-2" />*/}
                 Tělesa
               </Link>
             </li>
-            <li className="">
+            <li>
               <Link
                 to="/ukoly"
                 className="block w-full text-white text-xl px-4"
                 onClick={closeMenu}
               >
+                {/*<AiOutlineCheckCircle className="mr-2" />*/}
                 Úkoly
               </Link>
             </li>
@@ -141,61 +143,150 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+
+          <div className="mt-auto px-4">
+            {!authUser ? (
+              <Link
+                to="/prihlaseni"
+                className="navbutton text-white w-full px-4 py-2 text-xl rounded-full border hover:bg-gray-800 transition-all duration-300 flex justify-center items-center"
+                onClick={closeMenu}
+              >
+                Přihlášení
+              </Link>
+            ) : (
+              <Link
+                to="/"
+                className="text-red-500 w-full px-4 py-2 text-xl flex justify-center items-center"
+                onClick={() => {
+                  signOutUser();
+                  closeMenu();
+                }}
+              >
+                Odhlásit se
+              </Link>
+            )}
+          </div>
         </div>
       </div>
 
-      {/* Right Side: Login or Logout */}
-      <div className="relative dropdown-container">
-        {authUser ? (
-          <div className="relative">
-            <button
-              onClick={toggleDropdown}
-              className="dropdown-toggle text-white px-4 py-2 text-xl border-form lg:text-2xl flex justify-center items-center lg:px-10"
-            >
-              <div className="flex flex-row">
-                <p className="text-white pr-5">
-                  Dobrý den,
-                  {userData?.name ? ` ${userData.name} ` : authUser.email}
-                </p>
-                <img
-                  src={avatarUrl || "/default-avatar.jpg"} // Use the public URL from context
-                  className="w-10 h-10 object-fit-contain rounded-full"
-                  alt="Avatar"
-                />
-              </div>
-            </button>
-            {isDropdownOpen && (
-              <div className="absolute right-0 mt-2 w-52 usergradient text-white rounded-lg shadow-lg flex flex-col justify-start items-start font-bold z-50">
-                <Link to="/profil" className="p-2 w-full">
-                  <div className="w-40 border-b-2">
-                    <p className="text-2xl ">Účet</p>
-                  </div>
-                  <p className="pt-2">Profil</p>
-                </Link>
-                <Link to="/pomoc" className="p-2 w-full">
-                  Pomoc
-                </Link>
-                <Link to="/" className="p-2 w-full">
-                  <button
-                    onClick={signOutUser}
-                    className="text-red-800 p-2 w-full text-left"
-                  >
-                    Odhlásit se
-                  </button>
-                </Link>
-              </div>
-            )}
-          </div>
-        ) : (
-          <Link
-            to="/prihlaseni"
-            className="navbutton text-white w-30 md:w-40 lg:w-50 px-4 py-2 text-xl rounded-full border hover:bg-gray-800 transition-all duration-300 lg:text-2xl flex justify-center items-center lg:px-10"
+      {/* Original navbar for desktop */}
+      <nav className="fixed top-0 left-0 right-0 bg-transparent text-white px-4 py-2 flex justify-between items-center shadow-lg z-30 lg:relative lg:bg-transparent w-full">
+        {/* Left Side: Hamburger menu for mobile, horizontal menu for desktop */}
+        <div className="flex items-center">
+          {/* Hamburger Icon for mobile */}
+          <button
+            onClick={toggleMenu}
+            className="lg:hidden text-white focus:outline-none transition-transform transform duration-300 z-40"
           >
-            Přihlášení
-          </Link>
-        )}
+            <svg
+              className="w-7 h-7"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+
+          {/* Desktop horizontal menu */}
+          <div className="hidden lg:block lg:w-auto">
+            <ul className="lg:flex lg:space-x-6 font-bold">
+              <li className="flex items-center">
+                <Link to="/success" className="flex text-white text-2xl">
+                  <FaHome className="mr-2" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/telesa" className="text-white text-xl">
+                  Tělesa
+                </Link>
+              </li>
+              <li>
+                <Link to="/ukoly" className="text-white text-xl">
+                  Úkoly
+                </Link>
+              </li>
+              <li>
+                <Link to="/projekt" className="text-white text-xl">
+                  O projektu
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Right Side: Login or User Profile */}
+        <div className="relative dropdown-container">
+          {authUser ? (
+            <div className="relative">
+              <button
+                onClick={toggleDropdown}
+                className="dropdown-toggle text-white px-4 py-2 text-xl border-form lg:text-2xl flex justify-center items-center lg:px-10"
+              >
+                <div className="flex flex-row">
+                  <p className="text-white pr-5">
+                    Dobrý den,
+                    {userData?.name ? ` ${userData.name} ` : authUser.email}
+                  </p>
+                  <img
+                    src={avatarUrl || "/default-avatar.jpg"} // Use the public URL from context
+                    className="w-10 h-10 object-fit-contain rounded-full"
+                    alt="Avatar"
+                  />
+                </div>
+              </button>
+              {isDropdownOpen && (
+                <div className="absolute right-0 mt-2 w-52 usergradient text-white rounded-lg shadow-lg flex flex-col justify-start items-start font-bold z-50">
+                  <Link to="/profil" className="p-2 w-full">
+                    <div className="w-40 border-b-2">
+                      <p className="text-2xl ">Účet</p>
+                    </div>
+                    <p className="pt-2">Profil</p>
+                  </Link>
+                  <Link to="/pomoc" className="p-2 w-full">
+                    Pomoc
+                  </Link>
+                  <Link to="/" className="p-2 w-full">
+                    <button
+                      onClick={signOutUser}
+                      className="text-red-800 p-2 w-full text-left"
+                    >
+                      Odhlásit se
+                    </button>
+                  </Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <Link
+              to="/prihlaseni"
+              className="navbutton text-white w-30 md:w-40 lg:w-50 px-4 py-2 text-xl rounded-full border hover:bg-gray-800 transition-all duration-300 lg:text-2xl flex justify-center items-center lg:px-10"
+            >
+              Přihlášení
+            </Link>
+          )}
+        </div>
+      </nav>
+
+      {/* Main content area */}
+      <div className="flex-1 ml-0 lg:ml-0">
+        {/* This is where your page content would go */}
       </div>
-    </nav>
+
+      {/* Overlay for mobile menu */}
+      {isMenuOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"
+          onClick={closeMenu}
+        />
+      )}
+    </div>
   );
 }
 
